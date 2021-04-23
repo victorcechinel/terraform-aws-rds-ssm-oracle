912762186511505110c5f586c67fe4303d11cb20
Victor Cechinel <dev.victorcechinel@gmail.com>
2021-04-23 16:38:04 -0300

    :sparkles: Example create oracle

---
+++ examples/oracle/main.tf
@@ -0,0 +1,9 @@
+module "aws_rds_oracle" {
+  source  = "victorcechinel/rds-ssm-oracle/aws"
+  version = "1.0.3"
+
+  identifier             = "rds-identifier"
+  subnet_group           = "my-subnet-group"
+  parameter_group        = "my-parameter-group"
+  vpc_security_group_ids = ["vpc-group-id"]
+}
